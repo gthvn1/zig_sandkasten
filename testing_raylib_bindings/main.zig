cf5db7ce736335f9c5d05f74a9df52cab32e6e61
Guillaume <guillaume.thouvenin@vates.tech>
2024-08-13 15:38:14 +0200

    print debug infos and allow pausing the ball

--- testing_raylib_bindings/main.zig
+++ testing_raylib_bindings/main.zig
@@ -3,15 +3,18 @@ const rl = @import("raylib.zig");
 
 const Vector2 = rl.Vector2;
 
-pub fn main() void {
+pub fn main() !void {
     const title = "Testing Zig & Raylib";
     const win_width: isize = 800;
     const win_height: isize = 600;
 
+    var debug_buffer = [_]u8{0} ** 1024;
+    var paused = false;
+
     var ball_pos = Vector2{ win_width / 2, 20 };
     const ball_size: f32 = 10.0;
-    var velocity = Vector2{ 0, 500 };
-    const gravity = Vector2{ 0, 0.1 };
+    var velocity = Vector2{ 100, 500 };
+    const gravity = Vector2{ 0, 10 };
 
     const pink: rl.Color = .{
         .r = 0xFF,
@@ -20,16 +23,29 @@ pub fn main() void {
         .a = 0xFF,
     };
 
-    std.debug.print("Hello, Sailor!", .{});
     rl.init_window(win_width, win_height, title);
     defer rl.close_window();
 
-    while (!rl.window_should_close()) {
-        // Update
-        const dt: f32 = rl.get_frame_time();
+    rl.set_target_fps(60);
 
+    while (!rl.window_should_close()) {
+        rl.begin_drawing();
+        defer rl.end_drawing();
+
+        // Update
+        if (rl.is_space_pressed()) {
+            std.debug.print("SPACE pressed... ", .{});
+            paused = !paused;
+            std.debug.print("paused is set to {}\n", .{paused});
+        }
+
+        if (paused) {
+            continue;
+        }
+
+        const dt: f32 = rl.get_frame_time();
+        velocity += gravity; // velocity increases by acceleration due to gravity
         ball_pos += velocity * Vector2{ dt, dt };
-        velocity += gravity;
 
         if (ball_size >= ball_pos[1] or ball_pos[1] >= win_height - ball_size) {
             // We hit the top or the ground so inverse y velocity
@@ -41,11 +57,11 @@ pub fn main() void {
             // We hit the left or right so inverse x velocity
             velocity *= Vector2{ -0.9, 1 };
         }
+        const debug_slice: [:0]u8 = try std.fmt.bufPrintZ(&debug_buffer, "dt: {d:0<.2} ms\nvelocity: {d:0<.0}\nball_pos: {d:0<.0}", .{ dt, velocity, ball_pos });
 
         // Draw
-        rl.begin_drawing();
         rl.clear_background(rl.Color{ .r = 0x0, .g = 0x00, .b = 0x00, .a = 0xFF });
+        rl.draw_text(debug_slice, Vector2{ 10, 10 });
         rl.draw_circle_v(ball_pos, 10.0, pink);
-        rl.end_drawing();
     }
 }
--- testing_raylib_bindings/raylib.zig
+++ testing_raylib_bindings/raylib.zig
@@ -11,8 +11,17 @@ pub const Color = struct {
     a: u8,
 };
 
+pub fn set_target_fps(fps: usize) void {
+    const c_fps: c_int = @intCast(fps);
+    return raylib.SetTargetFPS(c_fps);
+}
+
 pub fn get_frame_time() f32 {
     return raylib.GetFrameTime();
+}
+
+pub fn is_space_pressed() bool {
+    return raylib.IsKeyPressed(raylib.KEY_SPACE);
 }
 
 pub fn init_window(width: isize, height: isize, title: [:0]const u8) void {
@@ -41,6 +50,14 @@ pub fn begin_drawing() void {
 
 pub fn end_drawing() void {
     raylib.EndDrawing();
+}
+
+pub fn draw_text(text: [:0]const u8, pos: Vector2) void {
+    const pos_x: c_int = @intFromFloat(pos[0]);
+    const pos_y: c_int = @intFromFloat(pos[1]);
+    const fs: c_int = 12;
+
+    raylib.DrawText(text.ptr, pos_x, pos_y, fs, raylib.WHITE);
 }
 
 pub fn draw_circle_v(center: Vector2, radius: f32, color: Color) void {
