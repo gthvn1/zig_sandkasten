f897e235e4c5f1ab00cd426287d566f215453735
Guillaume <guillaume.thouvenin@vates.tech>
2024-08-17 20:19:52 +0200

    copy implem of loggme

--- wasm_and_web_server/src/web_pages/fibo.zig
+++ wasm_and_web_server/src/web_pages/fibo.zig
@@ -7,13 +7,37 @@ pub const index =
     \\<body>
     \\    <p>check console output</p>
     \\    <script>
+    \\        var memory; // We need access to memory to read data like logs
+    \\
+    \\        function strlen(ptr) {
+    \\          let len = 0;
+    \\          // We don't expect string to have len greater than 1024 chars
+    \\          while (memory[ptr] != 0 && len < 1024) {
+    \\            len++;
+    \\            ptr++;
+    \\          }
+    \\
+    \\          return len;
+    \\        }
+    \\
+    \\        function loggme(ptr) {
+    \\          const len = strlen(ptr);
+    \\          console.log(len);
+    \\
+    \\          const buf = new Uint8Array(memory.buffer, ptr, len);
+    \\          //const str = new TextDecoder("utf8").decode(buf);
+    \\          const str = String.fromCharCode(...buf);
+    \\          console.log(str);
+    \\        }
+    \\
     \\        const imports = {
-    \\            loggme: () => console.log("Todo"),
+    \\            loggme,
     \\        };
     \\
     \\        WebAssembly
     \\            .instantiateStreaming(fetch("./fibo.wasm"), {env: imports})
     \\            .then((obj) => {
+    \\                memory = obj.instance.exports["memory"];
     \\                console.log(obj.instance.exports.fibo(10));
     \\            });
     \\    </script>
