98650c0c5f542448c528799db44fd5757d5d37eb
Guillaume <guillaume.thouvenin@vates.tech>
2024-08-17 16:24:57 +0200

    use build.zig for building

---
+++ wasm_and_web_server/build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+const Exe = struct {
+    name: []const u8,
+    source: []const u8,
+};
+
+pub fn build(b: *std.Build) void {
+    const t = b.standardTargetOptions(.{});
+    const executables = [_]Exe{
+        .{ .name = "app_fibo", .source = "app_fibo.zig" },
+        .{ .name = "web_fibo", .source = "web_fibo.zig" },
+    };
+
+    inline for (executables) |e| {
+        const exe = b.addExecutable(.{
+            .name = e.name,
+            .root_source_file = b.path(e.source),
+            .target = t,
+        });
+
+        b.installArtifact(exe);
+    }
+
+    // We also need to cross compile our fibo.zig to fibo.wasm...
+    // https://github.com/daneelsan/minimal-zig-wasm-canvas/blob/master/build.zig
+    const wasm = b.addExecutable(.{
+        .name = "fibo",
+        .root_source_file = b.path("./fibo.zig"),
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        }),
+        .optimize = .ReleaseSmall,
+    });
+    wasm.rdynamic = true;
+    wasm.entry = .disabled;
+
+    b.installArtifact(wasm);
+}
